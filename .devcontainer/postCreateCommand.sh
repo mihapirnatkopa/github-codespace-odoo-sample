f8313efeb3f1d42f63bcbc6876b447ea22037537
mihapirnatkopa <150355773+mihapirnatkopa@users.noreply.github.com>
2023-11-19 19:24:50 +0000

    debugpy

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,10 +3,14 @@
 # load enviroment variables from initializeCommand.sh
 source .devcontainer/.env
 
+# install Odoo App DebugPY for Visual Studio Code
+mkdir ../addons/odoo-vscode && git clone https://github.com/tosolini/odoo-vscode.git ../addons/odoo-vscode
+
+# stop odoo service until we set some things up
+sudo service odoo stop
+
 # local dev-container workaround
-if [[ $localWorkspaceFolder != "/var/lib/docker/codespacemount"* ]]; then
-    # stop odoo service until we set some things up
-    sudo service odoo stop
+if [[ $localWorkspaceFolder != "/var/lib/docker/codespacemount"* ]]; then    
 
     # remove existing links in /mnt/extra-addons 
     find /mnt/extra-addons -maxdepth 1 -type l -delete
@@ -17,10 +21,10 @@ if [[ $localWorkspaceFolder != "/var/lib/docker/codespacemount"* ]]; then
     # link odoo.conf
     sudo rm /etc/odoo/odoo.conf
     sudo ln -s ${localWorkspaceFolder}/odoo/config/odoo.conf /etc/odoo/
-    # start odoo service again
-    sudo service odoo start
 fi
 
+# start odoo service again
+sudo service odoo start
 
 # install python requirements
 sudo pip3 install -r ./requirements.txt
