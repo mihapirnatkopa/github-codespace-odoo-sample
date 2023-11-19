812bf8717479c034c143f81fc669d4507d3d77ed
mihapirnatkopa <150355773+mihapirnatkopa@users.noreply.github.com>
2023-11-19 18:06:59 +0000

    debugpy step 1

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,7 +4,7 @@
 source .devcontainer/.env
 
 # local dev-container workaround
-if [ -f "$localWorkspaceFolder" ]; then
+if [[ $localWorkspaceFolder != "/var/lib/docker/codespacemount"* ]]; then
     # stop odoo service until we set some things up
     sudo service odoo stop
 
@@ -14,8 +14,9 @@ if [ -f "$localWorkspaceFolder" ]; then
     # if /mnt/extra-addons is empty, create symbolic links to addons/* folders
     if [ -z \"$(ls -A /mnt/extra-addons)\" ]; then sudo ln -snf ${localWorkspaceFolder}/addons/* /mnt/extra-addons; fi
 
-    # copy odoo.conf
-    sudo cp ${localWorkspaceFolder}/odoo/config/odoo.conf /etc/odoo/
+    # link odoo.conf
+    sudo rm /etc/odoo/odoo.conf
+    sudo ln -s ${localWorkspaceFolder}/odoo/config/odoo.conf /etc/odoo/
 
     # start odoo service again
     sudo service odoo start
