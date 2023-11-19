550a85ad2539efb488dcf817e28167f4a535fc9f
mihapirnatkopa <150355773+mihapirnatkopa@users.noreply.github.com>
2023-11-19 19:05:10 +0000

    fixes oops

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -17,13 +17,15 @@ if [[ $localWorkspaceFolder != "/var/lib/docker/codespacemount"* ]]; then
     # link odoo.conf
     sudo rm /etc/odoo/odoo.conf
     sudo ln -s ${localWorkspaceFolder}/odoo/config/odoo.conf /etc/odoo/
-
     # start odoo service again
     sudo service odoo start
 fi
 
+
 # install python requirements
 sudo pip3 install -r ./requirements.txt
 
+# activate developer mode
+wget http://localhost:8069/web?debug=1 -nv -O -  > /dev/null
 
 
