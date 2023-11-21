38f37eee53790330a277a0cbf92bf2326bccdabf
mihapirnatkopa <150355773+mihapirnatkopa@users.noreply.github.com>
2023-11-21 11:38:32 +0000

    codespaces add link to odoo src

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -15,9 +15,11 @@ if [[ $localWorkspaceFolder != "/var/lib/docker/codespacemount"* ]]; then
     # link odoo.conf
     sudo rm /etc/odoo/odoo.conf
     sudo ln -s ${localWorkspaceFolder}/odoo/config/odoo.conf /etc/odoo/
+    (sleep 5 && sudo ln -snf /usr/lib/python3/dist-packages/odoo ${localWorkspaceFolder}/odoo/src) &
+
+else
+    (sleep 5 && sudo ln -snf /usr/lib/python3/dist-packages/odoo /workspace/odoo/src) &
 fi
 
 # install python requirements
 sudo pip3 install -r ./requirements.txt
-
-(sleep 5 && sudo ln -snf /usr/lib/python3/dist-packages/odoo ${localWorkspaceFolder}/odoo/src) &
