2112554e10b2a10d089dba8ce8ce7b473127d697
Miha Pirnat <miha.pirnat@kopa.si>
2023-11-23 09:23:31 +0000

    local dev container restart server when addons change

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,18 +5,11 @@ source .devcontainer/.env
 
 # local dev-container workaround
 if [[ $localWorkspaceFolder != "/var/lib/docker/codespacemount"* ]]; then    
-
-   # remove existing links in /mnt/extra-addons 
-    sudo find /mnt/extra-addons -maxdepth 1 -type l -delete
-
-    # if /mnt/extra-addons is empty, create symbolic links to addons/* folders
-    if [ -z "$(ls -A /mnt/extra-addons)" ]; then sudo ln -snf ${localWorkspaceFolder}/addons/* /mnt/extra-addons; fi
-    
     # link odoo.conf
     sudo rm /etc/odoo/odoo.conf
     sudo ln -s ${localWorkspaceFolder}/odoo/config/odoo.conf /etc/odoo/
     (sleep 5 && sudo ln -snf /usr/lib/python3/dist-packages/odoo ${localWorkspaceFolder}/odoo/src) &
-    screen -d -m odoo -c /etc/odoo/odoo.conf --dev=all --log-level=debug
+    screen -d -m env WS=${localWorkspaceFolder} bash -c 'while true; do odoo -c /etc/odoo/odoo.conf --dev=all --log-level=debug --addons-path ${WS}/addons/; sleep 1; done'
 else
     (sleep 5 && sudo ln -snf /usr/lib/python3/dist-packages/odoo /workspace/odoo/src) &
 fi
